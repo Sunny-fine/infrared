30e79a5a64f26fca0e1e12f84f756f7528678c41
Sunny-fine <104741564+Sunny-fine@users.noreply.github.com>
2022-05-05 13:36:48 +0900

    Add files via upload

---
+++ getTemp.php
@@ -0,0 +1,16 @@
+<?php
+ $d = json_decode(file_get_contents('php://input'), true);
+ #var_dump($d["x"]);
+ #if (!file_exists('/tmp/temp1.csv') {
+ #  shell_exec('cp ./temp1.csv /tmp/');
+ #}
+ #$spl = new SplFileObject('temp1.csv');
+ $spl = new SplFileObject('FLIR_9.csv');
+ $spl->seek($d["y"]*480+$d["x"]+1);
+ $csv = explode(",",$spl->current());
+ print_r("X: ".$csv[1].", "."Y: ".$csv[0].", "."T:".$csv[2]); 
+ #echo strval($data->x).' '.strval($data->y);
+ #print("=$data->x=\n");
+ #phpinfo();
+ #echo 'Hello, World!';
+?>
